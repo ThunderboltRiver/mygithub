312f14680cb7643b173a16f3f82208a49a826fb9
沖田大河 <lostbleuseas@outlook.jp>
2021-01-07 23:01:09 +0900

    making class go_persistence

--- persistence.py
+++ persistence.py
@@ -4,44 +4,41 @@ import numpy as np
 from sgfmill import sgf
 from scipy.spatial.distance import pdist
 
-def read_sgf_from_zip(zip_file, number):
-    winners = []
-    board_size = []
-    root_nodes = []
-    dic_nodes = {}
-    b_players = []
-    w_players = []
-    with zf.ZipFile(zip_file) as zip_data:
-        infos = [info for info in zip_data.infolist() if '.sgf' in info.filename]
-        for info in infos:
-            with zip_data.open(info.filename, 'r') as f:
-            	game = sgf.Sgf_game.from_bytes(f.read())
-            winners.append(game.get_winner())
-            board_size.append(game.get_size())
-            root_nodes.append(game.get_root())
-            b_players.append(root_nodes[-1].get('PB'))
-            w_players.append(root_nodes[-1].get('PW'))
-            file_name = info.filename.encode('cp437').decode('utf-8')
-            dic_nodes[file_name] = [node.get_move() for node in game.get_main_sequence() if node.get_move() != (None, None) ]
-        if number == None:
-            return {'win':winners,'size':board_size,'kihu':dic_nodes,'b':b_players,'w':w_players}
+class GO_persistence:
+	def __init__(self,zip_file):
+	    self.winners = []
+	    self.board_size = []
+	    self.kihus = []
+	    self.b_players = []
+	    sefl.w_players = []
+	    with zf.ZipFile(zip_file) as zip_data:
+	        infos = [info for info in zip_data.infolist() if '.sgf' in info.filename]
+	        for info in infos:
+	            with zip_data.open(info.filename, 'r') as f:
+	            	game = sgf.Sgf_game.from_bytes(f.read())
+	            self.winners.append(game.get_winner())
+	            self.board_size.append(game.get_size())
+	            root_node = game.get_root()
+	            self.b_players.append(root_node.get('PB'))
+	            self.w_players.append(root_node.get('PW'))
+	            self.kihus.append([node.get_move() for node in game.get_main_sequence() if node.get_move() != (None, None) ])
 
-        else:
-            file_name = infos[number].filename.encode('cp437').decode('utf-8')
-            return {'win':winners[number],'size':board_size[number],'kihu':{file_name:dic_nodes[file_name]},'b':b_players[number],'w':w_players[number]}
-
-
-def B_or_W(kihu_dict):
-    Black_list = []
-    White_list = []
-    for key in kihu_dict.keys():
-    	Black = np.array([goishi[1] for goishi in kihu_dict[key] if goishi[0] == 'b'])
-    	White = np.array([goishi[1] for goishi in kihu_dict[key] if goishi[0] == 'w'])
-    	Black_list.append(Black)
-    	White_list.append(White)
-
-
-    return Black_list, White_list
-
-def persistence()	
-
+	def B_or_W(self):
+	    Black_list = []
+	    White_list = []
+	    for kihu in self.kihus:
+	    	Black = np.array([goishi[1] for goishi in kihu if goishi[0] == 'b'])
+	    	White = np.array([goishi[1] for goishi in kihu if goishi[0] == 'w'])
+	    	Black_list.append(Black)
+	    	White_list.append(White)
+	
+	
+	    return Black_kihus, White_kihus
+	
+	def B_or_W_fill(self,dim, radius):
+		B, W = self.B_or_W()
+		B_fill_list = [d.fill_rips(b, dim, radius) for b in B]
+		W_fill_list = [d.fill_rips(w, dim, radius) for w in W]
+		
+		return B_fill_list, W_fill_list
+		
