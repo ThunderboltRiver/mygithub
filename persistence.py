b728e11f49e4cc4d096aee0bc8fce50c29ca6b59
ThunderboltRiver <lostbleuseas@outlook.jp>
2021-01-16 01:10:22 +0900

    add hist plot on plot_dim2filldgms

--- persistence.py
+++ persistence.py
@@ -27,38 +27,39 @@ class kihu_persistence:
             turns = step_turns(kihu, step)
             self.losers_dim2fill.append([d.fill_rips(pdist(kihu[: t]), dim, radius) for t in turns])
 
-    def random_choice_homology(self, choice_size = 1, replace = False ,compare = False, show = True, save = False, figpath = None):
+    def random_choice_homology(self, choice_size = 1, replace = False ,compare = False, show = True, save = False, figname_head = None):
         random_indexes = np.random.choice(self.num_games, choice_size, replace = replace)
-        return self.choice_homology(random_indexes, compare = compare, show = show, save = save, figpath = figpath)
+        return self.choice_homology(random_indexes, compare = compare, show = show, save = save, figname_head = figname_head)
 
 
-    def choice_homology(self, indexes, compare = False, show = True, save = False, figpath = None):
-        out = []
+    def choice_homology(self, indexes, compare = False, show = True, save = False, figname_head = None):
+        persons_dim2filldgms_list = []
+        persons = ['winner', 'loser']
         persons_dim2fill = [self.winners_dim2fill]
-        if compare == True:
+        if compare:
             persons_dim2fill.append(self.losers_dim2fill)
-        for dim2fill_list in persons_dim2fill:
-            dim2fill_dgms_lists = []
-            dim2fill_list = [dim2fill_list[i]  for i in indexes]
-            for count, dim2fill in enumerate(dim2fill_list):
-                print(f'loading dim2_fill {count}')
-                dim2fill_dgms = [d.init_diagrams(d.homology_persistence(fill), fill) for fill in dim2fill]
-                dim2fill_dgms_lists.append(dim2fill_dgms)
-            out.append(dim2fill_dgms_lists)
+        for person, dim2fill_list in enumerate(persons_dim2fill):
+            dim2filldgms_list = []
+            compute_dim2fill_list = [dim2fill_list[i]  for i in indexes]
+            for number, dim2fill in enumerate(compute_dim2fill_list):
+                print(f'computing dim2 filtration diagrams of {persons[person]} {number}')
+                dim2filldgms = [d.init_diagrams(d.homology_persistence(fill), fill) for fill in dim2fill]
+                dim2filldgms_list.append(dim2filldgms)
+            persons_dim2filldgms_list.append(dim2filldgms_list)
 
-        if show == True or save == True:
-            people_list = ['winner', 'loser']
-            for i, dim2fill_dgms_lists in enumerate(out):
-                plot_dim2fill_dgms(dim2fill_dgms_lists, title = f'{people_list[i]} step = {self.step} ', show = False)
-                if save == True:
-                    fig_name = f'{figpath}_{people_list[i]}{self.homology_count}'
-                    plt.savefig(fig_name)
-                    print(f'saved {fig_name}')
-            if show == True:
+        if show or save:
+            for person, dim2filldgms_list in enumerate(persons_dim2filldgms_list):
+                title = f'{persons[person]} step:{self.step} count:{self.homology_count}'
+                figname = None
+                if save:
+                    figname = f'{figname_head}{self.homology_count}_{persons[person]}'
+                    print(f'saving {figname}')
+                plot_dim2filldgms(dim2filldgms_list, title = title, show = (not compare) and show, save = save, figname = figname)
+            if compare and show:
                 plt.show()
             self.homology_count += 1
 
-        return out
+        return persons_dim2filldgms_list
 
 
 class GO_data:
@@ -114,30 +115,42 @@ def step_turns(kihu, step):
 
     return turns
 
-def plot_dim2fill_dgms(dim2fill_dgms_lists, title = None, show = True):
+def plot_dim2filldgms(dim2filldgms_list, title = None, show = True, save = False, figname = None):
     colors = ['r','g','b','c','m','y','k']
-    for number, dim2fill_dgms in enumerate(dim2fill_dgms_lists):
+    for number, dim2filldgms in enumerate(dim2filldgms_list):
         fig, axes = plt.subplots(4, 4, figsize = (12, 20))
         one_dim_axes = axes.ravel()
-        for i, dgms in enumerate(dim2fill_dgms):
+        for i, dgms in enumerate(dim2filldgms):
             for k, dgm in enumerate(dgms):
-                style = {'color':colors[k]}
-                if dgms == dim2fill_dgms[-1]:
-                    style['label'] = 'dim = ' + str(k)
-
+                color = colors[k]
+                label = None
+                style = {'color':color}
+                if dgms == dim2filldgms[-1]:
+                    style['label'] = f'dim = {k}'
+                    label = f'hist:dim = {k}, death = inf'
+                ax = one_dim_axes[i]
                 try:
-                    d.plot.plot_diagram(dgm,ax = one_dim_axes[i], pt_style = style)
+                    d.plot.plot_diagram(dgm,ax = ax, pt_style = style)
 
                 except ValueError:
+                    if len(dgm):
+                       birth_list = [hole.birth for hole in dgm if hole.death == float('inf')]
+                       bins_list = []
+                       for birth in birth_list:
+                           if birth not in bins_list:
+                               bins_list.append(birth)
+                       ax.hist(birth_list, bins = len(bins_list), density = True, color = color, label = label)
                     continue
 
         fig.legend()
-        fig.suptitle(f'number = {number} {title}')
-    if show == True:
+        fig.suptitle(f'{title} dim2 filtraion diagrams{number}')
+        if save:
+            plt.savefig(f'{figname}{number}')
+    if show:
         plt.show()
 
-def test(zip_path, choice_size, compare, show, save, figpath, repeat):
+def test(zip_path, choice_size, compare, show, save, figname_head, repeat):
     go = GO_data(zip_path)
     kp = kihu_persistence(go)
     for i in range(repeat):
-        kp.random_choice_homology(choice_size = choice_size, compare = compare, show = show, save = save, figpath = figpath)
+        kp.random_choice_homology(choice_size = choice_size, compare = compare, show = show, save = save, figname_head = figname_head)
