6ab6c45c9485591fe203b84e1770e0fdae1b7bb5
ThunderboltRiver <lostbleuseas@outlook.jp>
2021-03-11 17:09:24 +0900

    create julia module

--- igo_persistence/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-from numpy import get_include # cimport numpy を使うため
-
-ext = Extension("persistence", sources=["persistence.pyx"], include_dirs=['.', get_include()])
-setup(name="persistence", ext_modules=cythonize([ext]))
